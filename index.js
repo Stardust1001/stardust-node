64e549dc35a149ffc7b93f26a2abdfc8394a678c
Stardust <2368354416@qq.com>
2023-05-06 12:15:25 +0800

    add

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import fsUtils from './fsUtils.js'
 import funcs from './funcs.js'
 import storage from './storage.js'
+import websocket from './websocket.js'
 
 export {
   fsUtils,
   funcs,
-  storage
+  storage,
+  websocket
 }
 
 export default {
   fsUtils,
   funcs,
-  storage
+  storage,
+  websocket
 }
