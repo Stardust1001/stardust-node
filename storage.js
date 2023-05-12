c7176df03db61e3efb3774f50872cb171c107df9
Stardust <2368354416@qq.com>
2023-05-12 10:20:25 +0800

    add

--- storage.js
+++ storage.js
@@ -3,42 +3,49 @@ import { read, write } from './fsUtils.js'
 class Storage {
   constructor (config = {}) {
     this.config = config
-    const { filepath, autoLoad = false, jsonSpace = 2 } = config
+    const { filepath, autoLoad = false, autoSave = true, jsonSpace = 2 } = config
     this.filepath = filepath
+    this.autoLoad = autoLoad
+    this.autoSave = autoSave
     this.jsonSpace = jsonSpace
 
     this.loaded = false
-    this.storage = null
+    this.cache = null
 
     if (autoLoad) this.load()
   }
 
   async load () {
     const text = await read(this.filepath)
-    this.storage = JSON.parse(text || '{}')
-    return this.storage
+    this.cache = JSON.parse(text || '{}')
+    this.loaded = true
+    return this.cache
   }
 
-  async save (storage = {}) {
-    this.storage = storage
-    await write(this.filepath, JSON.stringify(storage, null, this.jsonSpace))
+  async save (cache) {
+    if (cache === undefined && !this.loaded) {
+      return
+    }
+    await this.laod()
+    cache = cache || this.cache
+    await write(this.filepath, JSON.stringify(cache, null, this.jsonSpace))
   }
 
   getItem (key) {
     if (!this.loaded) throw new Error('please load first')
-    return this.storage[key]
+    return this.cache[key]
   }
 
   setItem (key, value) {
     if (!this.loaded) throw new Error('please load first')
-    this.storage[key] = value
-    this.save(this.storage)
+    this.cache[key] = value
+    this.save(this.cache)
   }
 
   clear () {
     if (!this.loaded) throw new Error('please load first')
-    this.storage = {}
-    this.save(this.storage)
+    this.cache = {}
+    this.save(this.cache)
   }
 }
 
