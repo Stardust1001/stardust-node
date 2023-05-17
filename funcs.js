47288c4e4170c58c2f8ca75eceb54a1d37e7784e
Stardust <2368354416@qq.com>
2023-05-17 20:59:00 +0800

    add

--- funcs.js
+++ funcs.js
@@ -9,7 +9,19 @@ export const openUrl = url => {
   child_process.spawn(isWindows() ? 'explorer' : 'open', [url])
 }
 
+export const pipeRes = (res, ctx) => {
+  const headers = {}
+  Array.from(res.headers.entries()).forEach(([k, v]) => headers[k] = v)
+  delete headers['content-encoding']
+  ctx.res.writeHead(res.status, headers)
+  res.body.pipe(ctx.res)
+  return new Promise((resolve) => {
+    res.body.on('end', resolve)
+  })
+}
+
 export default {
   isWindows,
-  openUrl
+  openUrl,
+  pipeRes
 }
