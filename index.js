f012ca361ce5dcbd1f1fbe168660b92f4ef66f22
Stardust <2368354416@qq.com>
2023-05-23 21:55:51 +0800

    add

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-import dbsdk from './dbsdk/index.js'
 import fsUtils from './fsUtils.js'
 import funcs from './funcs.js'
 import Storage from './storage.js'
 
 export {
-  dbsdk,
   fsUtils,
   funcs,
   Storage
 }
 
 export default {
-  dbsdk,
   fsUtils,
   funcs,
   Storage
