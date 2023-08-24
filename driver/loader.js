f860842704905f8b5c5cb943b41cdcc2366f9065
Stardust <2368354416@qq.com>
2023-08-24 20:46:57 +0800

    add

--- driver/loader.js
+++ driver/loader.js
@@ -20,7 +20,7 @@ export class Loader {
   }
 
   static async excel (filepath, options = {}) {
-    let { sheetNames = ['Sheet1'] } = options
+    let { sheetNames = ['Sheet1'], withHidden = false } = options
     const workbook = new Excel.Workbook()
     await workbook.xlsx.readFile(filepath)
     if (sheetNames === '*') {
@@ -33,17 +33,17 @@ export class Loader {
       sheets: sheetNames.map(name => {
         const sheet = workbook.getWorksheet(name)
         const list = () => {
-          const rows = sheet.getSheetValues()
+          const rows = withHidden ? sheet.getSheetValues() : sheet._rows.filter(r => !r.hidden).map(r => r.values)
           for (let i = 0, rowCount = rows.length; i < rowCount; i++) {
             rows[i] ??= []
             for (let j = 0, cellCount = rows[i].length; j < cellCount; j++) {
               rows[i][j] = Loader.getCellValue(rows[i][j])
             }
           }
-          return rows
+          return rows.filter(r => r.length)
         }
         const json = () => {
-          const rows = list().filter(r => r.length)
+          const rows = list()
           const headerIndex = options.headerRowIndex || 0
           const header = rows[headerIndex]
           return rows.slice(headerIndex + 1).map(r => {
